2822ecc553255e2bcecf0d88978cee0f710afa14
iaminfadel <aminfadel2004@gmail.com>
2025-02-13 00:44:39 +0200

    refactor: chnage variable names to follow code formatting

--- src/teleop.py
+++ src/teleop.py
@@ -8,21 +8,21 @@ class ControlApp(Tk, object):
 
     def __init__(self) -> None:
         super(ControlApp, self).__init__()
-        self.init_node()
+        self.initNode()
         self.config()
-        self.init_keyboard()
+        self.initKeyboard()
         self.mainloop()
 
-    def init_node(self) -> None:
-        rospy.init_node("teleop_node")
-        self.velocitylf_publisher = rospy.Publisher('/wheel_lhs_front_velocity_controller/command', Float64, queue_size=10)
-        self.velocityrf_publisher = rospy.Publisher('/wheel_rhs_front_velocity_controller/command', Float64, queue_size=10)
-        self.velocitylr_publisher = rospy.Publisher('/wheel_lhs_rear_velocity_controller/command', Float64, queue_size=10)
-        self.velocityrr_publisher = rospy.Publisher('/wheel_rhs_rear_velocity_controller/command', Float64, queue_size=10)
-        self.velocitylm_publisher = rospy.Publisher('/wheel_lhs_mid_velocity_controller/command', Float64, queue_size=10)
-        self.velocityrm_publisher = rospy.Publisher('/wheel_rhs_mid_velocity_controller/command', Float64, queue_size=10)
+    def initNode(self) -> None:
+        rospy.initNode("teleop_node")
+        self.velocitylfPublisher = rospy.Publisher('/wheel_lhs_front_velocity_controller/command', Float64, queue_size=10)
+        self.velocityrfPublisher = rospy.Publisher('/wheel_rhs_front_velocity_controller/command', Float64, queue_size=10)
+        self.velocitylrPublisher = rospy.Publisher('/wheel_lhs_rear_velocity_controller/command', Float64, queue_size=10)
+        self.velocityrrPublisher = rospy.Publisher('/wheel_rhs_rear_velocity_controller/command', Float64, queue_size=10)
+        self.velocitylmPublisher = rospy.Publisher('/wheel_lhs_mid_velocity_controller/command', Float64, queue_size=10)
+        self.velocityrmPublisher = rospy.Publisher('/wheel_rhs_mid_velocity_controller/command', Float64, queue_size=10)
         
-    def init_keyboard(self) -> None:
+    def initKeyboard(self) -> None:
         self.bind("<KeyPress>", self.keydown)
         self.bind("<KeyRelease>", self.keyup)
 
@@ -30,46 +30,46 @@ class ControlApp(Tk, object):
         self.forw = 1.57
         self.stop = 0
         self.back = -1.57
-        self.speeds_msg = Int8MultiArray()
+        self.speedsMsg = Int8MultiArray()
 
     def keydown(self, event: Event) -> None:
         if event.keysym == "Up":
-            self.velocitylf_publisher.publish(self.forw)
-            self.velocityrf_publisher.publish(self.forw)
-            self.velocitylr_publisher.publish(self.forw)
-            self.velocityrr_publisher.publish(self.forw)
-            self.velocitylm_publisher.publish(self.forw)
-            self.velocityrm_publisher.publish(self.forw)
+            self.velocitylfPublisher.publish(self.forw)
+            self.velocityrfPublisher.publish(self.forw)
+            self.velocitylrPublisher.publish(self.forw)
+            self.velocityrrPublisher.publish(self.forw)
+            self.velocitylmPublisher.publish(self.forw)
+            self.velocityrmPublisher.publish(self.forw)
         elif event.keysym == "Down":
-            self.velocitylf_publisher.publish(self.back)
-            self.velocityrf_publisher.publish(self.back)
-            self.velocitylr_publisher.publish(self.back)
-            self.velocityrr_publisher.publish(self.back)
-            self.velocitylm_publisher.publish(self.back)
-            self.velocityrm_publisher.publish(self.back)
+            self.velocitylfPublisher.publish(self.back)
+            self.velocityrfPublisher.publish(self.back)
+            self.velocitylrPublisher.publish(self.back)
+            self.velocityrrPublisher.publish(self.back)
+            self.velocitylmPublisher.publish(self.back)
+            self.velocityrmPublisher.publish(self.back)
         elif event.keysym == "Left":
-            self.velocitylf_publisher.publish(self.forw)
-            self.velocityrf_publisher.publish(self.back)
-            self.velocitylr_publisher.publish(self.forw)
-            self.velocityrr_publisher.publish(self.back)
-            self.velocitylm_publisher.publish(self.forw)
-            self.velocityrm_publisher.publish(self.back)
+            self.velocitylfPublisher.publish(self.forw)
+            self.velocityrfPublisher.publish(self.back)
+            self.velocitylrPublisher.publish(self.forw)
+            self.velocityrrPublisher.publish(self.back)
+            self.velocitylmPublisher.publish(self.forw)
+            self.velocityrmPublisher.publish(self.back)
         elif event.keysym == "Right":
-            self.velocitylf_publisher.publish(self.back)
-            self.velocityrf_publisher.publish(self.forw)
-            self.velocitylr_publisher.publish(self.back)
-            self.velocityrr_publisher.publish(self.forw)
-            self.velocitylm_publisher.publish(self.back)
-            self.velocityrm_publisher.publish(self.forw)
+            self.velocitylfPublisher.publish(self.back)
+            self.velocityrfPublisher.publish(self.forw)
+            self.velocitylrPublisher.publish(self.back)
+            self.velocityrrPublisher.publish(self.forw)
+            self.velocitylmPublisher.publish(self.back)
+            self.velocityrmPublisher.publish(self.forw)
 
     def keyup(self, event: Event) -> None:
         if event.keysym in ["Up", "Down", "Left", "Right"]:
-            self.velocitylf_publisher.publish(self.stop)
-            self.velocityrf_publisher.publish(self.stop)
-            self.velocitylr_publisher.publish(self.stop)
-            self.velocityrr_publisher.publish(self.stop)
-            self.velocitylm_publisher.publish(self.stop)
-            self.velocityrm_publisher.publish(self.stop)
+            self.velocitylfPublisher.publish(self.stop)
+            self.velocityrfPublisher.publish(self.stop)
+            self.velocitylrPublisher.publish(self.stop)
+            self.velocityrrPublisher.publish(self.stop)
+            self.velocitylmPublisher.publish(self.stop)
+            self.velocityrmPublisher.publish(self.stop)
 
 if __name__ == "__main__":
     try:
